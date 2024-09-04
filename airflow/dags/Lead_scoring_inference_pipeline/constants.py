a0439b5c87bed2b02fabdfddbfab062c74828c7d
aswinj18 <aswinjshaji@gmail.com>
2024-09-04 09:16:59 +0530

    Completed Data Inference DAG

---
+++ airflow/dags/Lead_scoring_inference_pipeline/constants.py
@@ -0,0 +1,23 @@
+DB_PATH = "/home/airflow/dags/Lead_scoring_data_pipeline/"
+DB_FILE_NAME = "lead_scoring_data_cleaning.db"
+
+DB_FILE_MLFLOW = "sqlite:////home/Lead_scoring_mlflow_production.db"
+
+PREDICTION_DISTRIBUTION_FILE_PATH = "/home/airflow/dags/Lead_scoring_inference_pipeline/prediction_distribution.txt"
+INPUT_FEATURES_CHECK_FILE_PATH = "/home/airflow/dags/Lead_scoring_inference_pipeline/input_features_check.log"
+
+TRACKING_URI = "http://0.0.0.0:6006"
+
+# experiment, model name and stage to load the model from mlflow model registry
+MODEL_NAME = "Lead_scoring_model"
+STAGE = "Production"
+EXPERIMENT = "Lead_scoring_mlflow_production"
+
+# list of the features that needs to be there in the final encoded dataframe
+ONE_HOT_ENCODED_FEATURES = [ "city_tier", "first_platform_c_", "first_utm_medium_c_", 
+                            "first_utm_source_c_", 'total_leads_droppped', 'referred_lead',
+                            'assistance_interaction', 'career_interaction',
+                            'payment_interaction', 'social_interaction', 'syllabus_interaction' ]
+
+# list of features that need to be one-hot encoded
+FEATURES_TO_ENCODE = [ "first_platform_c", "first_utm_medium_c", "first_utm_source_c" ]
---
+++ airflow/dags/Lead_scoring_inference_pipeline/lead_scoring_inference_pipeline.py
@@ -0,0 +1,74 @@
+##############################################################################
+# Import necessary modules
+# #############################################################################
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+from Lead_scoring_inference_pipeline.utils import *
+
+###############################################################################
+# Define default arguments and create an instance of DAG
+# ##############################################################################
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024,9,1),
+    'retries' : 1, 
+    'retry_delay' : timedelta(seconds=5)
+}
+
+
+Lead_scoring_inference_dag = DAG(
+                dag_id = 'Lead_scoring_inference_pipeline',
+                default_args = default_args,
+                description = 'Inference pipeline of Lead Scoring system',
+                schedule_interval = '@hourly',
+                catchup = False
+)
+
+###############################################################################
+# Create a task for encode_data_task() function with task_id 'encoding_categorical_variables'
+# ##############################################################################
+task_encode_features = PythonOperator(
+    task_id = "encode_features",
+    python_callable = encode_features,
+    dag = Lead_scoring_inference_dag
+)
+
+
+###############################################################################
+# Create a task for load_model() function with task_id 'generating_models_prediction'
+# ##############################################################################
+task_get_models_prediction = PythonOperator(
+    task_id = "get_models_prediction",
+    python_callable = get_models_prediction,
+    dag = Lead_scoring_inference_dag
+)
+
+
+###############################################################################
+# Create a task for prediction_col_check() function with task_id 'checking_model_prediction_ratio'
+# ##############################################################################
+task_prediction_ratio_check = PythonOperator(
+    task_id = "prediction_ratio_check",
+    python_callable = prediction_ratio_check,
+    dag = Lead_scoring_inference_dag
+)
+
+
+###############################################################################
+# Create a task for input_features_check() function with task_id 'checking_input_features'
+# ##############################################################################
+task_input_features_check = PythonOperator(
+    task_id = "input_features_check",
+    python_callable = input_features_check,
+    dag = Lead_scoring_inference_dag
+)
+
+
+###############################################################################
+# Define relation between tasks
+# ##############################################################################
+task_encode_features >> task_input_features_check >> task_get_models_prediction >> task_prediction_ratio_check
